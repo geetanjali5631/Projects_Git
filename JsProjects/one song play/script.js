b1016cc59fa6ff1e27877ff577598292ab67d26e
geetanjali5631 <geeta123chaudhary123@gmail.com>
2024-01-07 20:55:23 +0530

    Added 5 songs playlist linked togather

--- JsProjects/one song play/script.js
+++ JsProjects/one song play/script.js
@@ -1,32 +1,33 @@
-let progress = document.getElementById("progress")
-let song = document.getElementById("song")
-let ctrlIcon = document.getElementById("ctrl-icon")
+let progress = document.getElementById('progress')
+    let song = document.getElementById('song')
+    let ctrlIcon = document.getElementById('ctrl-icon')
 
-song.onloadedmetadata = function(){
-    progress.max = song.duration;
-    progress.value = song.currentTime;
-}
-function playPause(){
-    if(ctrlIcon.classList.contains("fa-pause")){
-        song.pause();
-        ctrlIcon.classList.remove("fa-pause");
-        ctrlIcon.classList.add("fa-play")
-    }
-    else{
-        song.play();
-        ctrlIcon.classList.add("fa-pause");
-        ctrlIcon.classList.remove("fa-play")
-    }
-}
-if(song.play()){
-    setInterval(()=>{
+    song.onloadedmetadata = function(){
+        progress.max = song.duration;
         progress.value = song.currentTime;
-    },500);
-}
+    }
+    function playPause(){
+      if(ctrlIcon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlIcon.classList.add("fa-play")
+        ctrlIcon.classList.remove("fa-pause")
+      }else{
+        song.play();
+        ctrlIcon.classList.add("fa-pause")
+        ctrlIcon.classList.remove("fa-play")
+      }
+    }
+    if(song.play()){
+        setInterval(()=>{
+            progress.value=song.currentTime;
+        },500)
+    }
+    progress.onchange= function(){
+        song.play();
+        song.currentTime=progress.value;
+        ctrlIcon.classList.remove("fa-play")
+        ctrlIcon.classList.add("fa-pause")
+    }
 
-progress.onchange = function(){
-    song.play();
-    song.currentTime = progress.value;
-    ctrlIcon.classList.add("fa-pause");
-    ctrlIcon.classList.remove("fa-play")
-}
+    let theme = document.getElementsByClassName('fa-bars');
+    
